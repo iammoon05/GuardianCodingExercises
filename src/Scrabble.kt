e4d1da20f8403ae5e5083e1b95e5bd4b80f3ab14
Abdullah Al Mamun <abdullahrens@gmail.com>
2025-10-19 00:13:10 +0200

    function for createDistributedRack

--- src/Scrabble.kt
+++ src/Scrabble.kt
@@ -1,8 +1,8 @@
 class Scrabble {
 
     fun calculateScore(inputWord: String, scoreMap: HashMap<Char, Int>): Int {
-        var inputCharArray: CharArray = inputWord.uppercase().toCharArray()
-        var score: Int = 0
+        val inputCharArray: CharArray = inputWord.uppercase().toCharArray()
+        var score = 0
         for (c in inputCharArray) {
             score += scoreMap[c] ?: 0
         }
@@ -10,9 +10,18 @@ class Scrabble {
     }
 
     fun createDistributedArray(distributionMap: HashMap<Char, Int>): CharArray {
-        var result = ArrayList<Char>()
-        for ((key, val) in distributionMap) {
-            result.add()
+        val result = ArrayList<Char>()
+        for ((key, value) in distributionMap) {
+            repeat(value) { result.add(key) }
         }
+        return result.toCharArray()
+    }
+
+    fun createDistributedRack(rackLength: Int, distArray: CharArray) : CharArray {
+        val result = CharArray(rackLength)
+        for (i in 0..rackLength) {
+            result[i] = distArray.random()
+        }
+        return result
     }
 }
